d8319a17d0297b1952ad42bbe76fe03e0fcf9e14
z9877969 <z9877969@ukr.net>
2021-07-14 14:48:13 +0300

    adds start structure for react-app

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ type = "button", title, cbOnClick }) => {
-  return (
-    <button tupe={type} onClick={cbOnClick}>
-      {title}
-    </button>
-  );
-};
-
-export default Button;
